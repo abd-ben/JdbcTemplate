e0e73fd2e3ccf2a8a058673e7bd3094659b100fc
abd-ben <abd.ben.ka@gmail.com>
2020-03-27 16:08:28 +0100

    abd-ben | test5 google-java-format

--- src/main/java/com/example2809/jdbcexemple/JdbcexempleApplication.java
+++ src/main/java/com/example2809/jdbcexemple/JdbcexempleApplication.java
@@ -1,18 +1,13 @@
 package com.example2809.jdbcexemple;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class   JdbcexempleApplication{
+public class JdbcexempleApplication {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
 
-                        SpringApplication.run(JdbcexempleApplication.class, args);
-
-                                            }
-
-
-                    }
+    SpringApplication.run(JdbcexempleApplication.class, args);
+  }
+}
